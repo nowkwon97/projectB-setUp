6712ff464322420d6e9a335cc516ba4624b4bdb2
leejaekwon <nowkwon97@gmail.com>
2023-06-23 11:04:46 +0900

    ADD react 번들링 파일 nest에서 불러오는 환경 구축

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,25 +5,34 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '../server/public'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
+  mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: './src/index.html',
+    }),
+  ],
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import path, { join } from 'path';
+import * as express from 'express'; // 추가
+// import * as path from 'path'; // 추가
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // 정적 파일 제공 설정 추가
+  // app.use(
+  //   '/static',
+  //   express.static(path.join(__dirname, '../../client/public')),
+  // );
+
   await app.listen(3000);
 }
 bootstrap();
