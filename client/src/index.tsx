81ea0bd23204f977e8367efb3b0a07a105dc5b00
leejaekwon <nowkwon97@gmail.com>
2023-06-25 13:24:30 +0900

    UPDATE 주석 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,3 @@
-// import React from 'react';
-// import * as ReactDOM from 'react-dom';
-// // 함수형 컴포넌트 타입을 명시하는 React.FC
-// const App: React.FC = () => {
-//   return <h1>Hello World!!!!</h1>;
-// };
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import React, { useEffect, useState } from 'react';
 import * as ReactDOM from 'react-dom';
 import axios from 'axios';
